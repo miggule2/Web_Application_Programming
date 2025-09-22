3b428ff30f1959f4be42f61950a8ec24f2fa46fd
miggule2 <miggul2@naver.com>
2025-09-22 13:25:48 +0900

    Feat : 3개의 요소 append 후 전체 순회 테스트

--- src/linkedList.ts
+++ src/linkedList.ts
@@ -1,20 +1,55 @@
-export class linkedList<T> {
+export class LinkedList<T> {
     head : Node<T> | null;
+    tail : Node<T> | null;
     
     constructor(){
-        head = null;
+        this.head = null;
+        this.tail = null;
     }
 
-    append.
+    append(value : T){
+        let node = new Node<T>(value);
+        
+        if(this.head == null || this.tail == null){
+            this.head = node;
+            this.tail = node;
+            return;
+        } 
+
+        this.tail.next = node;
+        node.prev = this.tail;
+        this.tail = node;
+        this.tail.next = null;
+    }
+
+    size() : number{
+        let size = 0;
+        let current = this.head;
+        while(current != null){
+            size++;
+            current = current.next;
+        }
+        return size;
+    }
+
+    printList() : T []{
+        let array:Array<T> = [];
+        let current = this.head;
+        while(current != null){
+            array.push(current.value);
+            current = current.next;
+        }
+        return array;
+    }
 }
 
 class Node<T>{
-    value : T | null;
+    value : T;
     prev : Node<T> | null;
     next : Node<T> | null;
 
-    constructor(){
-        this.value = null;
+    constructor(value : T){
+        this.value = value;
         this.prev = null;
         this.next = null;
     }
